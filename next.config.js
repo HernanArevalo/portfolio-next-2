7c226c79513c2046eabd2b3493bf3fa4bd7513a2
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-08-24 19:55:06 -0300

    add error paths

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/en',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'query',
+            key: 'path',
+            value: '(?!es|en).*'
+          },
+        ],
+        destination: '/en',
+        permanent: true,
+      },
+    ];
+  },
+}
 
 module.exports = nextConfig
