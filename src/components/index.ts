ed2a4a3f77517f29f96bf065b11b2257d999b3a0
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-08-26 17:38:50 -0300

    add maintenance add MeSection

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,6 @@ export * from './ui/language/Language'
 export * from './ui/likes/Likes'
 export * from './ui/navbar/Navbar'
 export * from './ui/rounded-button/RoundedButton'
+
 export * from './sections/HomeSection'
+export * from './sections/MeSeciton'
