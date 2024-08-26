e79926674c2224708ed495885bb7741d4f235bfb
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-08-26 17:08:06 -0300

    finish language component structure

--- src/components/ui/language/Language.tsx
+++ src/components/ui/language/Language.tsx
@@ -1,29 +1,17 @@
 "use client";
+import clsx from "clsx";
+import { usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
 
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
-interface Props {
-  params: {
-      lang: string
-  }
-}
-
-
-export const Language = ({ params }: Props) => {
+export const Language = () => {
   const router = useRouter();
+  const pathname = usePathname();
 
-  const { lang } = params
-
-  useEffect(() => {
-    if (!['en','es'].includes(lang)) {
-      router.push('en')
-    }
-  }, [ router, lang ])
-  
+  const [lang, setLang] = useState(pathname)
 
   const onChangeLanguage = (newLang: string) => {
-    router.push(`/${newLang}`)
+    setLang(newLang)
+    router.push(newLang)
   }
 
 
@@ -31,16 +19,18 @@ export const Language = ({ params }: Props) => {
     <div className="text-negro font-bold absolute top-2 left-1 rounded-lg bg-gris text-xs border-gris border-2 overflow-hidden">
 
       <div
-        className={`absolute bg-blanco w-3/6 h-full transition-all duration-300 ${lang === 'en' ? 'translate-x-0' : 'translate-x-full'
-          }`}
+        className={clsx('absolute bg-blanco w-3/6 h-full transition-all duration-300',
+                  lang === '/en'? 'translate-x-0' : 'translate-x-full'
+                  )
+          }
         style={{zIndex: 1 }}
       />
 
       <div className="relative flex flex-row justify-center items-center">
-        <div className='relative z-10 px-2 py-1 cursor-pointer' onClick={()=>{onChangeLanguage('en')}}>
+        <div className='relative z-10 px-2 py-1 cursor-pointer' onClick={()=>{onChangeLanguage('/en')}}>
           EN
         </div>
-        <div className='relative z-10 px-2 py-1 cursor-pointer' onClick={()=>{onChangeLanguage('es')}}>
+        <div className='relative z-10 px-2 py-1 cursor-pointer' onClick={()=>{onChangeLanguage('/es')}}>
           ES
         </div>
 
