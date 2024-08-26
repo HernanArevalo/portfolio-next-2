133b1c3aa9d8bc35cc30577f7277066715b8fe54
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-08-26 17:07:51 -0300

    finish likes component structure

--- src/components/ui/likes/Likes.tsx
+++ src/components/ui/likes/Likes.tsx
@@ -1,19 +1,49 @@
 "use client";
-import { useState } from 'react'
-import { BiHeart } from 'react-icons/bi'
+import { useLikesStore } from '@/store';
+import MotionNumber from 'motion-number';
+import { useEffect } from 'react'
+import { FaHeart, FaRegHeart } from 'react-icons/fa';
 
 export const Likes = () => {
 
-  const [isFilled, setIsFilled] = useState(false)
+  const { likes, loadLikes, addLike, removeLike, loading, loaded, liked } = useLikesStore()
+
+  const onClick = ()=>{
+    if (!loading) {
+      if (!liked) {
+        addLike();
+      }else{
+        removeLike();
+      }
+    }
+  }
+
+  useEffect(() => {
+    loadLikes();
+  }, [loadLikes])
+  
+
 
   return (
-    <div className="absolute right-5 top-6 bg-blanco text-negro rounded-2xl p-2 flex flex-row gap-1 justify-center items-center font-bold px-3 likes-container
-                    after:border-t-8 after:border-t-blanco after:top-10 after:absolute after:w-0 after:h-0 after:border-l-8 after:border-r-8 after:border-l-transparent after:border-r-transparent
-                   " 
-         onClick={()=>{setIsFilled(!isFilled)}}>
-        <BiHeart color={isFilled ? 'red' : 'gray'} fill='true'/>
-      35
+    <>
+      {loaded && 
+      <div className="absolute right-5 top-6 bg-blanco text-negro rounded-2xl p-2 flex flex-row gap-1 justify-center items-center font-bold px-3 likes-container cursor-pointer
+                      animate__animated animate__flipInY
+                      after:border-t-8 after:border-t-blanco after:top-9 after:absolute after:w-0 after:h-0 after:border-l-8 after:border-r-8 after:border-l-transparent after:border-r-transparent
+                    " 
+          onClick={onClick}>
+          { liked?
+            <FaHeart />
+            :
+            <FaRegHeart />
+          }
+          <MotionNumber
+              value={likes}
+              format={{ notation: 'compact'}}
+              locales="es-MX"
+          />
 
-    </div>
+      </div>}
+    </>
   )
 }
