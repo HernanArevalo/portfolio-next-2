a0ef4279ed84018bd3eca2c95ddc238c001f2ab0
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-08-27 18:14:01 -0300

    start MeSection

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,6 @@ export * from './ui/language/Language'
 export * from './ui/likes/Likes'
 export * from './ui/navbar/Navbar'
 export * from './ui/rounded-button/RoundedButton'
-export * from './sections/HomeSection'
+
+export * from './sections/1_HomeSection'
+export * from './sections/2_MeSection'
